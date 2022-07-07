10249d5e5e549c861468f656c113eea401f7d16f
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-07 15:25:57 +0300

    -

--- Book.php
+++ Book.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://kit.fontawesome.com/191f749b6c.js" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +12,11 @@
 </head>
 <body class="LoginRegisterPage">
    
+<div class="sidenav">
+  <a href="#services"><i class="fa-solid fa-house"></i> Home</a>
+  <a href="#clients"><i class="fa-solid fa-ticket"></i> My tickets</a>
+  <a href="#contact"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+</div>
 
 </body>
 </html>
