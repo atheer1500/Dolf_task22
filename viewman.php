4c02b728866dd90336374f452f197d669931ff38
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-18 11:36:31 +0300

    -

--- viewman.php
+++ viewman.php
@@ -3,7 +3,7 @@
 session_start();
 include('connection.php'); 
 $conn=OpenCon();
- $sqlforview = "SELECT `Title` FROM `event` WHERE `EventID`=  (SELECT `EventID` FROM `add_delete` WHERE `MangerID`='" .$_SESSION['MangerID'] ."');";  
+ $sqlforview = "SELECT `Title` FROM `events` WHERE `EventID`=  (SELECT `EventID` FROM `edit_event` WHERE `MangerID`='" .$_SESSION['MangerID'] ."');";  
  if ($result = mysqli_query($conn, $sqlforview))  
  {
  while($rowforview =mysqli_fetch_array($result)) 
