80979c0ce193308e5fd6ff4fe5f30d7895e47902
atheer1500 <atheer1500@gmail.com>
2022-07-19 10:22:00 +0300

    a

--- AdminAccount.php
+++ AdminAccount.php
@@ -47,8 +47,10 @@ opacity: 0.2;
 <body class="homePage admin">
 
 <div class="sidenav">
-    <a href="AdminHome.php"><i class="fa-solid fa-house"></i> Home</a>
-    <a href="AdminNewActor.php"><i class="fa-solid fa-circle-plus"></i> Add Actor</a>
+<a href="AdminHome.php"><i class="fa-solid fa-house"></i> Home</a>
+  <a href="AdminAccount.php"><i class="fa-solid fa-user"></i> My Account</a>
+
+  <a href="AdminNewActor.php"><i class="fa-solid fa-circle-plus"></i> Add Actor</a>
   <a href="AdminNewManager.php"><i class="fa-solid fa-circle-plus"></i> Add Manager</a>
   <a href="index.html"><i class="fa-solid fa-right-from-bracket"></i> Logout</a> <!--here we can move to php page that excute logout then header(location: index.html)-->
 </div>
