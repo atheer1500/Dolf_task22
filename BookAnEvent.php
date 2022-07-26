db30de6dcd14042dfe368dc27cc69302136cf90a
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-26 13:48:39 +0300

    -

--- BookAnEvent.php
+++ BookAnEvent.php
@@ -1,16 +1,26 @@
 <?php
 
+if (isset($_POST['Tickets'])&&isset($_POST['Payment'])) 
+{
 session_start();
 include('connection.php'); 
 $conn=OpenCon();
+$newTicketsNumber=$_POST['Tickets']-$_SESSION['AvailableTickets__'];
+$sqlInsertToBook="INSERT INTO `book`(`PaymentMethod`, `NumberOfTickets__`, `UserEmail`, `EventID`)
+VALUES ('".$_POST['Payment']."','".$_POST['Tickets']."','".$_SESSION["userID"]."','".$_SESSION['EventID']."');";
 
-  
-if (isset($_POST['Tickets'])) {
-    echo $_POST['Tickets'];
+$sqlUpdateEvent="UPDATE `events` SET `AvailableTickets__`='".$newTicketsNumbe."' WHERE `EventID`='".$_SESSION['EventID']."';";
+if ($ResultSqlInsertToBook= mysqli_query($conn, $SqlInsertToBook)&&$ResultSqlUpdateEvent= mysqli_query($conn, $SqlUpdateEvent))  
+{
+  echo "Booked sucssfly ";
+}
+else  {
+    printf("Error: %s\n", mysqli_error($conn));
+    exit();
+}
 }
 
-//$sqlInsertToBook="INSERT INTO `book`(`PaymentMethod`, `NumberOfTickets__`, `UserEmail`, `EventID`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]')
-//";
+
 ?>
 <!-- <html>
 <script>
