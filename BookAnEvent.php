8133b6dcab113467afff8f3c63a016fee0d55696
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-26 13:59:13 +0300

    -

--- BookAnEvent.php
+++ BookAnEvent.php
@@ -5,11 +5,13 @@ if (isset($_POST['Tickets'])&&isset($_POST['Payment']))
 session_start();
 include('connection.php'); 
 $conn=OpenCon();
-$newTicketsNumber=($_POST['Tickets'])-($_SESSION['AvailableTickets__']);
-$sqlInsertToBook="INSERT INTO `book`(`PaymentMethod`, `NumberOfTickets__`, `UserEmail`, `EventID`)
+// $booked=$_POST['Tickets'];
+// $Available=$_SESSION['AvailableTickets__'];
+// $newTicketsNumber=($booked)-($Available);
+$SqlInsertToBook="INSERT INTO `book`(`PaymentMethod`, `NumberOfTickets__`, `UserEmail`, `EventID`)
 VALUES ('".$_POST['Payment']."','".$_POST['Tickets']."','".$_SESSION["userID"]."','".$_SESSION['EventID']."');";
 
-$sqlUpdateEvent="UPDATE `events` SET `AvailableTickets__`='".$newTicketsNumbe."' WHERE `EventID`='".$_SESSION['EventID']."';";
+$SqlUpdateEvent="UPDATE `events` SET `AvailableTickets__`='".($_SESSION['AvailableTickets__']-$_POST['Tickets'])."' WHERE `EventID`='".$_SESSION['EventID']."';";
 if ($ResultSqlInsertToBook= mysqli_query($conn, $SqlInsertToBook)&&$ResultSqlUpdateEvent= mysqli_query($conn, $SqlUpdateEvent))  
 {
   echo "Booked sucssfly ";
