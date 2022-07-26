e0f3b4d2283cc4ba20009de2d8f666fa8af9164d
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-26 14:21:46 +0300

    -

--- BookAnEvent.php
+++ BookAnEvent.php
@@ -13,6 +13,8 @@ $SqlUpdateEvent="UPDATE `events` SET `AvailableTickets__`='".($_SESSION['Availab
 if ($ResultSqlInsertToBook= mysqli_query($conn, $SqlInsertToBook)&&$ResultSqlUpdateEvent= mysqli_query($conn, $SqlUpdateEvent))  
 {
     echo"
+    <link rel='stylesheet' href='CSS/Maincss.css' media='all' type='text/css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
           <style>
     
             td {
