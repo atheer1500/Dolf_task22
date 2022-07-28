873d358a8dbbfeb44726bc749dac8c5694d990b8
MaisaaAhmadAli <MaisaaAhmadAli@outlook.com>
2022-07-28 11:37:17 +0300

    -

--- NewUserRegseter.php
+++ NewUserRegseter.php
@@ -11,8 +11,19 @@
         $Gender=$_POST['Gender'];
         $Email=$_POST['Email'];
         $password=$_POST['password'];
-        
 
         $_SESSION["id"]=$Email;
+        $sqlInsertNewUser="INSERT INTO `end_user` (`FirstName`, `LastName`, `Password`, `UserEmail`, `Gender`) VALUES
+         ('".$FirstName."','".$LastName."','". $password."','". $Email."','".$Gender."')";
+         if(   mysqli_query($conn, $sqlInsertNewUser)  )
+         {
+            header("HomePage.php");
+         }
+         else
+         {
+            printf("Error: %s\n", mysqli_error($conn));
+            exit();
+         }
+
      }
        ?> 
