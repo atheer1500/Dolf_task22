3c97cb638a2a1048ece4e9da9958ff98b6334162
atheer1500 <atheer1500@gmail.com>
2022-07-28 13:29:20 +0300

    a

--- HomePage.php
+++ HomePage.php
@@ -95,8 +95,8 @@ text-align: left;
  .pic1{ 
  position: absolute; 
  z-index: 5; 
- top: 25%; 
- left:25%; 
+ top: 35%; 
+ left:15%; 
  } 
 
 </style>
@@ -233,7 +233,7 @@ echo "opacity: 0.2;";
 echo '" class="pic1">';
 
 if($row[5]=='0')
-echo "<img src='sold_out_PNG6.png' width='100px' class='pic1'>";
+echo "<img src='200 (2).gif' width='150px' class='pic1'>";
 
 echo '<p class ="events" style="font-size: medium; font-weight: bold;">
 '
