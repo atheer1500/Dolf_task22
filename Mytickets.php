2a25939f621c297fb8bc3927f264b93e7b10830e
atheer1500 <atheer1500@gmail.com>
2022-08-02 13:17:38 +0300

    a

--- Mytickets.php
+++ Mytickets.php
@@ -7,18 +7,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/Maincss.css" media="all" type="text/css">
-    <link rel="stylesheet" href="CSS/user.css" media="all" type="text/css">
+    <link rel="stylesheet" href="CSS/Maincss.css?v=<?php echo time(); ?>" media="all" type="text/css">
+    <link rel="stylesheet" href="CSS/user.css?v=<?php echo time(); ?>" media="all" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>View conserts</title>
-    <script>
-      // function Logout()
-      // {
-      //    <?php
-      //    include_once("Logout.php");
-      //    ?>
-      // }
-      </script>
+
 
 </head>
 <body >
